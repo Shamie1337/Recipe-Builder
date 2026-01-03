0fee30e3e8f66c9f1f3996ae933c1a684ad240db
Shamie1337 <nikolashalamanov999@gmail.com>
2026-01-03 19:40:29 +0200

    deleted the base and redid it from scratch.

--- RecipeBuilder/RecipeBuilder.API/obj/Debug/net9.0/RecipeBuilder.API.AssemblyInfo.cs
+++ RecipeBuilder/RecipeBuilder.API/obj/Debug/net9.0/RecipeBuilder.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeBuilder.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5106e5496644e102f74bb4a41102225c4557ec2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b181986ea1b89f78fe15af72f397a09575f24f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeBuilder.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeBuilder.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
